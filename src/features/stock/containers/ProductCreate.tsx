35c869d3b9330e60e7720c3b73ef3077ef230fdb
Satit Rakkhaphan <junior.codemobiles@gmail.com>
2022-07-24 23:28:26 +0700

    -

--- src/features/stock/containers/ProductCreate.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { useParams } from 'react-router-dom'
-
-const ProductCreate: React.FC = () => {
-  const params = useParams()
-  return <div>Product Create {params.productId}</div>
-}
-
-export default ProductCreate
--- src/features/stock/containers/ProductList.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-const ProductList: React.FC = () => {
-  return <div>Product List</div>
-}
-
-export default ProductList
